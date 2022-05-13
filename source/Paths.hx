c825ad8019240f6b8be5ace9ea796f27cf60c4cb
ShowoffTH <95130098+Idklol55@users.noreply.github.com>
2022-05-13 22:27:25 +0700

    🤔

--- source/Paths.hx
+++ source/Paths.hx
@@ -221,7 +221,7 @@ class Paths
 	
 	static public function getTextFromFile(key:String, ?ignoreMods:Bool = false):String
 	{
-		#if desktop
+		#if android
 		#if MODS_ALLOWED
 		if (!ignoreMods && FileSystem.exists(mods(key)))
 			return File.getContent(mods(key));
