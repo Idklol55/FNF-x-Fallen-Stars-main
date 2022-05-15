1d20c77b2dbab6e6a9e62a86acbcf925423757dc
ShowoffTH <95130098+Idklol55@users.noreply.github.com>
2022-05-15 14:17:54 +0700

    Update Song.hx

--- source/Song.hx
+++ source/Song.hx
@@ -5,7 +5,7 @@ import haxe.Json;
 import haxe.format.JsonParser;
 import lime.utils.Assets;
 
-#if MODS_ALLOWED
+#if sys
 import sys.io.File;
 import sys.FileSystem;
 #end
@@ -58,7 +58,7 @@ class Song
 		
 		var formattedFolder:String = Paths.formatToSongPath(folder);
 		var formattedSong:String = Paths.formatToSongPath(jsonInput);
-		#if MODS_ALLOWED
+		#if (desktop && MODS_ALLOWED)
 		var moddyFile:String = Paths.modsJson(formattedFolder + '/' + formattedSong);
 		if(FileSystem.exists(moddyFile)) {
 			rawJson = File.getContent(moddyFile).trim();
@@ -66,7 +66,7 @@ class Song
 		#end
 
 		if(rawJson == null) {
-			#if MODS_ALLOWED
+			#if desktop
 			rawJson = File.getContent(Paths.json(formattedFolder + '/' + formattedSong)).trim();
 			#else
 			rawJson = Assets.getText(Paths.json(formattedFolder + '/' + formattedSong)).trim();
